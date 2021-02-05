7ccee5fc7e60719c0675123ce8c5e5a390cdab5f
Amol <amoljmu@hotmail.com>
2021-02-06 02:57:13 +0800

    Added configuration for ng2 chart for customer count by years

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,16 @@
+/* eslint-disable no-console */
 import { Component, OnInit } from '@angular/core';
 
 import { LoginService } from 'app/core/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
+
+import { CustomerService } from 'app/entities/customer/customer.service';
+import { HttpResponse } from '@angular/common/http';
+import { ICustomer } from 'app/shared/model/customer.model';
+import { JhiEventManager } from 'ng-jhipster';
+import { Subscription } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'jhi-home',
@@ -12,10 +20,103 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit {
   account: Account | null = null;
 
-  constructor(private accountService: AccountService, private loginService: LoginService) {}
+  public customers: ICustomer[] = [];
+  eventSubscriber?: Subscription;
+  public year: Array<number> = [];
+  public cnt: Array<number> = [];
+
+  public SystemName = 'Customer Count';
+  public ChartType = 'bar' as const;
+  firstCopy = false;
+
+  public lineChartData: Array<number> = this.cnt;
+  public lineChartLabels: Array<any> = this.year;
+  public labelMFL: Array<any> = [{ data: this.lineChartData, label: this.SystemName }];
+  public lineChartOptions: any = {
+    responsive: true,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            max: 10,
+            min: 0,
+          },
+        },
+      ],
+      xAxes: [{}],
+    },
+    plugins: {
+      datalabels: {
+        display: true,
+        align: 'top',
+        anchor: 'end',
+        color: '#222',
+        font: {
+          family: 'FontAwesome',
+          size: 14,
+        },
+      },
+      deferred: false,
+    },
+  };
+
+  _lineChartColors: Array<any> = [
+    {
+      backgroundColor: 'red',
+      borderColor: 'red',
+      pointBackgroundColor: 'red',
+      pointBorderColor: 'red',
+      pointHoverBackgroundColor: 'red',
+      pointHoverBorderColor: 'red',
+    },
+  ];
+
+  constructor(
+    private accountService: AccountService,
+    private loginService: LoginService,
+    protected customerService: CustomerService,
+    protected eventManager: JhiEventManager
+  ) {}
+
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
+  loadAll(): void {
+    this.customerService.query().subscribe((res: HttpResponse<ICustomer[]>) => {
+      this.customers = res.body || [];
+      if (res.ok) this.otherFunction();
+    });
+  }
+
+  otherFunction(): void {
+    const map: Map<number, number> = new Map<number, number>();
+    for (let i = 0; i < this.customers.length; i++) {
+      const a = this.customers[i].dateOfBirth;
+      const b = +moment(a).format('YYYY');
+      let c: number = map.get(b) || 0;
+      c = c + 1;
+      map.set(b, c++);
+    }
+    const mapAsc = new Map([...map.entries()].sort());
+    mapAsc.forEach((value: number, key: number) => {
+      console.log('Amol-1--------------------map[' + key + ']:' + value);
+      this.year.push(key);
+      this.cnt.push(value);
+    });
+  }
+
+  registerChangeInCustomers(): void {
+    this.eventSubscriber = this.eventManager.subscribe('customerListModification', () => this.loadAll());
+  }
 
   ngOnInit(): void {
     this.accountService.identity().subscribe(account => (this.account = account));
+    this.loadAll();
+    this.registerChangeInCustomers();
   }
 
   isAuthenticated(): boolean {
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,8 +5,14 @@ import { JhipcustfinalSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  imports: [JhipcustfinalSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [JhipcustfinalSharedModule, RouterModule.forChild([HOME_ROUTE]), BrowserModule, HttpClientModule, FormsModule, ChartsModule],
+
   declarations: [HomeComponent],
 })
 export class JhipcustfinalHomeModule {}
